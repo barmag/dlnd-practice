04a056a0c8f2a785d606c7f01e876d8f94ac8ad6
Yasser Makram <yasser@barm.ag>
2017-08-24 09:58:16 +0200

    input and output layer

--- sentiment-analysis/sentiment_analysis.py
+++ sentiment-analysis/sentiment_analysis.py
@@ -41,12 +41,55 @@ for term, count in total_count.most_common():
     if count > 100:
         ratio = positive_count[term] / float(negative_count[term]+1)
         positive_negative_ratio[term] = np.log(ratio)
-print("Pos-to-neg ratio for 'the' = {}".format(positive_negative_ratio["the"]))
-print("Pos-to-neg ratio for 'amazing' = {}".format(positive_negative_ratio["amazing"]))
-print("Pos-to-neg ratio for 'terrible' = {}".format(positive_negative_ratio["terrible"]))
+#print("Pos-to-neg ratio for 'the' = {}".format(positive_negative_ratio["the"]))
+#print("Pos-to-neg ratio for 'amazing' = {}".format(positive_negative_ratio["amazing"]))
+#print("Pos-to-neg ratio for 'terrible' = {}".format(positive_negative_ratio["terrible"]))
 
-print("most common positive: ")
-print(positive_negative_ratio.most_common()[0:29])
-print("most common negative: ")
-print(positive_negative_ratio.most_common()[-31:-1])
+#print("most common positive: ")
+#print(positive_negative_ratio.most_common()[0:29])
+#print("most common negative: ")
+#print(positive_negative_ratio.most_common()[-31:-1])
+
+#unique set of vocabulary
+vocab = set(total_count.keys())
+
+layer_0 = np.zeros((1, len(vocab)))
+
+# Create a dictionary of words in the vocabulary mapped to index positions
+# (to be used in layer_0)
+word2index = {}
+for i,word in enumerate(vocab):
+    word2index[word] = i
+
+def update_input_layer(review):
+    """ Modify the global layer_0 to represent the vector form of review.
+    The element at a given index of layer_0 should represent
+    how many times the given word occurs in the review.
+    Args:
+        review(string) - the string of the review
+    Returns:
+        None
+    """
+     
+    global layer_0
+    
+    # clear out previous state, reset the layer to be all 0s
+    layer_0 *= 0
+    
+    # count how many times each word is used in the given review and store the results in layer_0 
+    for word in review.split(" "):
+        layer_0[0][word2index[word]] += 1
+
+def get_target_for_label(label):
+    """Convert a label to `0` or `1`.
+    Args:
+        label(string) - Either "POSITIVE" or "NEGATIVE".
+    Returns:
+        `0` or `1`.
+    """
+    if(label == 'positive'):
+        return 1
+    else:
+        return 0
+
     
