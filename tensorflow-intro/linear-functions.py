32fdd2ab5f1d205dc0ffe9adef3d3c5a50d911d0
Yasser Makram <yasser@barm.ag>
2017-08-28 10:27:44 +0200

    calc cross entropy

--- tensorflow-intro/linear-functions.py
+++ tensorflow-intro/linear-functions.py
@@ -13,5 +13,15 @@ bias = tf.Variable(tf.zeros(n_labels))
 # softmax
 x = tf.nn.softmax([2.0, 1.0, 0.2])
 
+softmax_data = [0.7, 0.2, 0.1]
+one_hot_data = [1.0, 0.0, 0.0]
+
+softmax = tf.placeholder(tf.float32)
+one_hot = tf.placeholder(tf.float32)
+
+# cross entropy
+cross_entropy = -tf.reduce_sum(tf.multiply(one_hot, tf.log(softmax)))
+
 with tf.Session() as sess:
     sess.run(init)
+    print(sess.run(cross_entropy, feed_dict={softmax: softmax_data, one_hot: one_hot_data}))
