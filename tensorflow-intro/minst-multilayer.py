049f5c8de910db0af8c0f3690c717cea9a0f9765
Yasser Makram <yasser@barm.ag>
2017-08-29 06:43:46 +0200

    train on mnist dataset with multilayer network

---
+++ tensorflow-intro/minst-multilayer.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets(".", one_hot=True, reshape=False)
+# hyper-parameters
+learning_rate = 0.001
+training_epoch = 20
+batch_size = 128
+display_step = 1
+
+n_inputs = 784
+n_classes = 10
+
+n_hidden_layer = 256    # layer number of features
+
+weights = [tf.Variable(tf.random_normal([n_inputs, n_hidden_layer])),
+                       tf.Variable(tf.random_normal([n_hidden_layer, n_classes]))]
+
+biases = [tf.random_normal([n_hidden_layer]), tf.random_normal([n_classes])]
+
+x = tf.placeholder(tf.float32, [None, 28, 28, 1])
+y = tf.placeholder(tf.float32, [None, n_classes])
+x = tf.reshape(x, [-1, n_inputs])
+
+# hidden layer ReLU activation
+layer_1 = tf.add(tf.matmul(x, weights[0]), biases[0])
+layer_1 = tf.nn.relu(layer_1)
+
+# output layer with linear activation
+logits = tf.add(tf.matmul(layer_1, weights[1]), biases[1])
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    # training cycle
+    for epoch in range(training_epoch):
+        total_batch = int(mnist.train.num_examples/batch_size)
+        for i in range(total_batch):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            # run training
+            sess.run(optimizer, feed_dict={x: np.reshape(batch_x, [-1, n_inputs]), y: batch_y})
+    
+    
