610f2f22c59eac7e49067b0f0e10257b8accbb31
Yasser Makram <yasser@barm.ag>
2017-09-01 09:06:30 +0200

    generator for batches

--- kaRNNa_excrcise/kaRNNa_excrcise.py
+++ kaRNNa_excrcise/kaRNNa_excrcise.py
@@ -13,3 +13,32 @@ vocab_to_int = {c: i for i, c in enumerate(vocab)}
 encoded = np.array([vocab_to_int[c] for c in text])
 print(text[:100])
 print(encoded[:100])
+
+def get_batches(arr, n_seqs, n_steps):
+    """
+    Create a generator that returns batches of size
+    n_seqs, x n_steps from array
+    Arguments
+    ---------
+    arr: Array of int represented characters to get batches from
+    n_seqs: Batch size, the number of sequences per batch
+    n_steps: number of sequence steps per batch
+    """
+    # number of chars per batch and number of batches
+    chars_per_batch = n_seqs * n_steps
+    n_batches = len(arr)//chars_per_batch
+
+    # trim array to exactly number of bataches (no padding)
+    arr = arr[:n_batches*chars_per_batch]
+
+    # reshape into number of seqs rows
+    arr = np.reshape(arr, (n_seqs, -1))
+    for n in range(0, arr.shape[1], n_steps):
+        x = arr[:, n:+n_steps]
+        # labels (x shifted by 1)
+        y = np.zeros_like(x)
+        y[:, :-1], y[:, -1] = x[:, 1:], x[:, 0]
+        yield x, y
+
+batches = get_batches(encoded, 10, 50)
+x, y = next(batches)
