4eef7dc9a7c15d87dcee05be1bf9e8e33ee58adb
Yasser Makram <yasser@barm.ag>
2017-09-09 18:43:03 +0200

    get batches

--- Project3Spikes/Project3Spikes.py
+++ Project3Spikes/Project3Spikes.py
@@ -80,6 +80,81 @@ def build_rnn(cell, inputs):
     final_state = tf.identity(final_state, 'final_state')
     return outputs, final_state
 
+def build_nn(cell, rnn_size, input_data, vocab_size, embed_dim):
+    """
+    Build part of the neural network
+    :param cell: RNN cell
+    :param rnn_size: Size of rnns
+    :param input_data: Input data
+    :param vocab_size: Vocabulary size
+    :param embed_dim: Number of embedding dimensions
+    :return: Tuple (Logits, FinalState)
+    """
+    embed = get_embed(input_data, vocab_size, embed_dim)
+    
+    outputs, final_state = build_rnn(cell, embed)
+    print(outputs.shape)
+    logits = tf.contrib.layers.fully_connected(outputs, vocab_size, activation_fn=None)
+    print(logits.shape)
+    return logits, final_state
+
+def get_batches(int_text, batch_size, seq_length):
+    """
+    Return batches of input and target
+    :param int_text: Text with the words replaced by their ids
+    :param batch_size: The size of batch
+    :param seq_length: The length of sequence
+    :return: Batches as a Numpy array
+    """
+    #print(len(int_text))
+    batch_seq_size = batch_size*seq_length
+    num_batches = len(int_text)//(batch_size*seq_length)
+    int_text = int_text[:num_batches*(batch_size*seq_length)]
+    print(len(int_text))
+    output = np.zeros((num_batches, 2, batch_size, seq_length), dtype=np.int)
+    for batch in range(0, num_batches):
+        x = [[int_text[x+y] for x in range(0, seq_length)] for y in range(batch*seq_length, (num_batches*batch_seq_size)-(seq_length*(num_batches-batch-1)), seq_length*num_batches)]
+        y = [[int_text[x+y+1] if (x+y+1)<len(int_text) else int_text[0] for x in range(0, seq_length)] for y in range(batch*seq_length, (num_batches*batch_seq_size)-(seq_length*(num_batches-batch-1)), seq_length*num_batches)]
+        #print(x)
+        #print(y)
+        output[batch, 0, :, :] = x
+        output[batch, 1, :, :] = y
+
+    #print(output.shape)
+    return output
+
+def test_get_batches(get_batches):
+    with tf.Graph().as_default():
+        test_batch_size = 128
+        test_seq_length = 5
+        test_int_text = list(range(1000*test_seq_length))
+        batches = get_batches(test_int_text, test_batch_size, test_seq_length)
+
+        # Check type
+        assert isinstance(batches, np.ndarray),\
+            'Batches is not a Numpy array'
+
+        # Check shape
+        assert batches.shape == (7, 2, 128, 5),\
+            'Batches returned wrong shape.  Found {}'.format(batches.shape)
+
+        for x in range(batches.shape[2]):
+            assert np.array_equal(batches[0,0,x], np.array(range(x * 35, x * 35 + batches.shape[3]))),\
+                'Batches returned wrong contents. For example, input sequence {} in the first batch was {}'.format(x, batches[0,0,x])
+            assert np.array_equal(batches[0,1,x], np.array(range(x * 35 + 1, x * 35 + 1 + batches.shape[3]))),\
+                'Batches returned wrong contents. For example, target sequence {} in the first batch was {}'.format(x, batches[0,1,x])
+
+
+        last_seq_target = (test_batch_size-1) * 35 + 31
+        last_seq = np.array(range(last_seq_target, last_seq_target+ batches.shape[3]))
+        last_seq[-1] = batches[0,0,0,0]
+
+        assert np.array_equal(batches[-1,1,-1], last_seq),\
+            'The last target of the last batch should be the first input of the first batch. Found {} but expected {}'.format(batches[-1,1,-1], last_seq)
+
+    print("get batches success")
+
+
 text = '''
         Moe_Szyslak Moe's Tavern Where the elite meet to drink
         Bart_Simpson Eh yeah hello is Mike there Last name Rotch
@@ -91,3 +166,9 @@ text = '''
         Barney_Gumble Yeah you should only drink to enhance your social skills'''
 
 # create_lookup_tables(text)
+test_input_data_shape = [128, 5]
+test_vocab_size = 27
+
+print(test_input_data_shape + [test_vocab_size])
+
+test_get_batches(get_batches)
