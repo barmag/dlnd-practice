0347750816e9502624e87a3464abdbccf0042e14
Yasser Makram <yasser@barm.ag>
2017-09-12 08:01:17 +0200

    model inputs

--- project4-mt-spikes/project4_mt_spikes.py
+++ project4-mt-spikes/project4_mt_spikes.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 def text_to_ids(source_text, target_text, source_vocab_to_int, target_vocab_to_int):
     """
     Convert source and target text to proper word ids
@@ -15,3 +17,23 @@ def text_to_ids(source_text, target_text, source_vocab_to_int, target_vocab_to_i
     target_id_text = [[target_vocab_to_int[word] for word in sentence.split()] for sentence in target_sentences]
     
     return source_id_text, target_id_text
+
+def model_inputs():
+    """
+    Create TF Placeholders for input, targets, learning rate, and lengths of source and target sequences.
+    :return: Tuple (input, targets, learning rate, keep probability, target sequence length,
+    max target sequence length, source sequence length)
+    """
+    inputs = tf.placeholder(tf.int32, [None, None], name="input")
+    targets = tf.placeholder(tf.int32, [None, None], name="target")
+
+    learning_rate = tf.placeholder(tf.float32, name="learning_rate")
+    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+
+    target_seq_length = tf.placeholder(tf.int32, (None,), name="target_sequence_length")
+    max_target_length = tf.reduce_max(target_seq_length, name="max_target_length")
+    source_seq_length = tf.placeholder(tf.int32, (None,), name="source_sequence_length")
+
+    return inputs, targets, learning_rate, keep_prob, target_seq_length, max_target_length, source_seq_length
+
+print("tf loaded!")
