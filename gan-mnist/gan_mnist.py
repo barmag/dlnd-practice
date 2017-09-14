e6b2e9c8ffd96fa3b9b83369af93fe960e4561e4
Yasser Makram <yasser@barm.ag>
2017-09-14 17:06:46 +0200

    model inputs

--- gan-mnist/gan_mnist.py
+++ gan-mnist/gan_mnist.py
@@ -6,4 +6,10 @@ import matplotlib.pyplot as plt
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data')
 
+def model_inputs(real_dim, z_dim):
+    inputs_real = tf.placeholder(tf.float32, [None, real_dim], name='input_real')
+    inputs_z = tf.placeholder(tf.float32, [None, z_dim], name='input_z')
+    
+    return inputs_real, inputs_z
+
 print("modules loaded")
