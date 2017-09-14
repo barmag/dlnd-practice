fa8d57b18b0a3366361df6769c50e5baa07ab92a
Yasser Makram <yasser@barm.ag>
2017-09-14 18:29:20 +0200

    generator network

--- gan-mnist/gan_mnist.py
+++ gan-mnist/gan_mnist.py
@@ -12,4 +12,32 @@ def model_inputs(real_dim, z_dim):
     
     return inputs_real, inputs_z
 
-print("modules loaded")
+def generator(z, out_dim, n_units=128, reuse=False,  alpha=0.01):
+    ''' Build the generator network.
+    
+        Arguments
+        ---------
+        z : Input tensor for the generator
+        out_dim : Shape of the generator output
+        n_units : Number of units in hidden layer
+        reuse : Reuse the variables with tf.variable_scope
+        alpha : leak parameter for leaky ReLU
+        
+        Returns
+        -------
+        out: 
+    '''
+    def leaky_relu(input):
+        return tf.maximum(input, alpha*input)
+
+    with tf.variable_scope('generator', reuse=reuse): # finish this
+        # Hidden layer
+        h1 = tf.layers.dense(z, n_units, activation=None)
+        # Leaky ReLU
+        h1 = leaky_relu(h1)
+        
+        # Logits and tanh output
+        logits = tf.layers.dense(h1, out_dim, activation=None)
+        out = tf.tanh(logits)
+        
+        return out
