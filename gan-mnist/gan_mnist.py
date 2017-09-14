0e9b3a88d91722543bf87e11395edd60d0ab1ebf
Yasser Makram <yasser@barm.ag>
2017-09-14 18:48:32 +0200

    GAN hyperparameters

--- gan-mnist/gan_mnist.py
+++ gan-mnist/gan_mnist.py
@@ -66,3 +66,15 @@ def discriminator(x, n_units=128, reuse=False, alpha=0.01):
         out = tf.sigmoid(logits)
         
         return out, logits
+
+# Size of input image to discriminator
+input_size = 784 # 28x28 MNIST images flattened
+# Size of latent vector to generator
+z_size = 100
+# Sizes of hidden layers in generator and discriminator
+g_hidden_size = 128
+d_hidden_size = 128
+# Leak factor for leaky ReLU
+alpha = 0.01
+# Label smoothing 
+smooth = 0.1
