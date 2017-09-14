79ddad4791b3142356d064a92ff0ea2ff9994323
Yasser Makram <yasser@barm.ag>
2017-09-14 16:46:36 +0200

    load modules and read mnist dataset

---
+++ gan-mnist/gan_mnist.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# load mnist dataset
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data')
+
+print("modules loaded")
